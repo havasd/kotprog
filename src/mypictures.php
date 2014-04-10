60e565dc0a5378c5627d6c8e9c8617f5cefc4282
David Havas <havasd@inf.u-szeged.hu>
2014-04-10 14:14:02 +0200

    Added files.

---
+++ src/mypictures.php
@@ -0,0 +1,30 @@
+           <div class="example">
+                    <div id="anyad" class="tab-control" data-role="tab-control">
+                        <ul class="tabs">
+                            <li class="active"><a href="#_page_1">Tab 1</a></li>
+                            <li><a href="#_page_2">Other Tab</a></li>
+                            <li><a href="#_page_3"><i class="icon-rocket"></i></a></li> 
+                            <li class="place-right"><a href="#_page_5"><i class="icon-heart"></i></a></li>
+                            <li class="place-right"><a href="#_page_4"><i class="icon-cog"></i></a></li> 
+                        </ul>
+
+                        <div class="frames">
+                            <div class="frame" id="_page_1">
+                                <p>First Tab</p>
+                            </div>
+                            <div class="frame" id="_page_2">
+                                <p>Second Tab</p>
+                            </div>
+                            <div class="frame" id="_page_3">
+                                <p>Rocket Tab</p>
+                            </div>
+                            <div class="frame" id="_page_4">
+                                <p>This tab placed right</p>
+                            </div>
+                            <div class="frame" id="_page_5">
+                                <p>This tab also placed right</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
