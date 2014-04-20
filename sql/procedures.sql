9c5815f1c34130e7be8485180420a37df17b268b
Blaymoira <blaymoira@tauri.hu>
2014-04-20 12:02:44 +0200

    Removed unused procedure

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -53,17 +53,3 @@ BEGIN
   create_time := TO_CHAR(c_time, 'YYYY/MM/DD HH24:MI:SS');
   INSERT INTO ALBUMOK (ID, NEV, LEIRAS, LETREHOZAS_IDEJE, FELH_ID) VALUES (id, name, description, c_time, user_id);
 END;
-
---------------------------------------------------------
---  Album creator PROCEDURE
---------------------------------------------------------
-create or replace PROCEDURE upload_picture
-(user_id IN number, album_id IN NUMBER, cat_id IN NUMBER, description IN varchar2, place IN varchar2, pic OUT BLOB, id OUT number, create_time OUT STRING)
-IS
-  c_time DATE := SYSDATE;
-BEGIN
-  id := image_seq.nextval;
-  create_time := TO_CHAR(c_time, 'YYYY/MM/DD HH24:MI:SS');
-  INSERT INTO KEPEK (ID, LEIRAS, FELTOLTES_IDEJE, HELYSZIN, KEPFAJL, ALBUM_ID, FELH_ID, KAT_ID) VALUES (id, description, c_time, place, EMPTY_BLOB(), album_id, user_id, cat_id)
-  RETURN KEPFAJL INTO pic;
-END;
