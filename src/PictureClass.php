6fc18dbb3700a7ffdf334c47b3b5e9cab731ad87
Blaymoira <blaymoira@tauri.hu>
2014-04-20 12:01:41 +0200

    Added missing files from previous commit.

---
+++ src/PictureClass.php
@@ -0,0 +1,8 @@
+<?php
+    class Picture
+    {
+        $m_id;
+        $m_desc;
+        $m_owner;
+    }
+?>
---
+++ src/pictureupload.php
@@ -0,0 +1,25 @@
+<?php
+    require_once('UserClass.php');
+    session_start();
+    $usr = $_SESSION['userObject'];
+    if (isset($usr)) {
+        if (isset($_FILES[0])) {
+            $blob = base64_encode(file_get_contents($_FILES[0]['tmp_name']));
+            if ($usr->uploadPicture($blob))
+                echo json_encode(array('create' => 'true'));
+            else
+                echo json_encode(array('create' => 'false'));
+        } else {
+            echo '<form id="f_new_pictures" style="margin: 5px 5px 5px 5px">
+                    <div class="grid fluid show-grid">
+                        <div class="row">
+                            <input type="file" name="file_picture" id="in_file_picture"/><br>
+                        </div>
+                        <div class="row">
+                            <input type="submit" id="btn_upload_picture" value="Feltöltés">
+                        </div>
+                    </div>
+                </form>';
+        }
+    }
+?>
